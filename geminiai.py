fd05f170ae9d5ed416d83bcc9f9cf017d56ec71d
dhiyzl <86110711+itsdzl@users.noreply.github.com>
2026-06-13 01:52:00 +0700

    Update geminiai.py

--- geminiai.py
+++ geminiai.py
@@ -132,13 +132,13 @@ async def allmsg(m):
                 result = eval(cmd, globals(), local_vars)
                 
             await bot.reply_to(m, f"💡 *Result:* \n`{result}`", parse_mode="Markdown")
-                except Exception:
+        except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             err = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
             err_msg = "❌ *Error:* \n
 http://googleusercontent.com/immersive_entry_chip/0
-```"
 
+```"
             await bot.reply_to(m, err_msg, parse_mode="Markdown")
         return
 
